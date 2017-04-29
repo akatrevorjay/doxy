2f7e32b034dd1ef9b58baf63c71c55499b66b0e9
Trevor Joynson (trevorj) <github@skywww.net>
2017-04-29 02:03:15 -0700

    mrclean

--- servers/http_proxy.go
+++ servers/http_proxy.go
@@ -71,10 +71,27 @@ func setCA(caCert, caKey []byte) error {
 		return err
 	}
 	goproxy.GoproxyCa = goproxyCa
-	goproxy.OkConnect = &goproxy.ConnectAction{Action: goproxy.ConnectAccept, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
-	goproxy.MitmConnect = &goproxy.ConnectAction{Action: goproxy.ConnectMitm, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
-	goproxy.HTTPMitmConnect = &goproxy.ConnectAction{Action: goproxy.ConnectHTTPMitm, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
-	goproxy.RejectConnect = &goproxy.ConnectAction{Action: goproxy.ConnectReject, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
+
+	goproxy.OkConnect = &goproxy.ConnectAction{
+		Action: goproxy.ConnectAccept,
+		TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa),
+	}
+
+	goproxy.MitmConnect = &goproxy.ConnectAction{
+		Action: goproxy.ConnectMitm,
+		TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa),
+	}
+
+	goproxy.HTTPMitmConnect = &goproxy.ConnectAction{
+		Action: goproxy.ConnectHTTPMitm,
+		TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa),
+	}
+
+	goproxy.RejectConnect = &goproxy.ConnectAction{
+		Action: goproxy.ConnectReject,
+		TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa),
+	}
+
 	return nil
 }
 
